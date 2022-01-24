e5474b19f9c4d0a550130336c742629caf941b3a
Criste Phillips <criste.phillips@yahoo.com>
2022-01-24 18:28:17 -0500

    lesson 1 codechecked

--- __tests__/random.test.js
+++ __tests__/random.test.js
@@ -1,11 +1,11 @@
-//require the module that testing is against, using relative path of location
-const randomNumber = require('../lib/random.js');
+// //require the module that testing is against, using relative path of location
+// const randomNumber = require('../lib/random.js');
 
-//requires 2 arguments
-// argument 1 = a descriptive string stating what the outcomes are for this test
-test('gets random number between 1 and 10', () => {
-    // arguement 2 = call back fxn that runs the test, 
-        // here () =>   through expect....
-    expect(randomNumber()).toBeGreaterThanOrEqual(1);
-    expect(randomNumber()).toBeLessThanOrEqual(10);
-});
+// //requires 2 arguments
+// // argument 1 = a descriptive string stating what the outcomes are for this test
+// test('gets random number between 1 and 10', () => {
+//     // arguement 2 = call back fxn that runs the test, 
+//         // here () =>   through expect....
+//     expect(randomNumber()).toBeGreaterThanOrEqual(1);
+//     expect(randomNumber()).toBeLessThanOrEqual(10);
+// });
